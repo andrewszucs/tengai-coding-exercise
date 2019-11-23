7425abfb43adec249528cdd7d7cb18e729dda36e
Andrew Szucs <andrew0szucs@gmail.com>
2019-11-23 22:31:40 +0100

    Implement the Header

--- src/App/components/Header/Header.js
+++ src/App/components/Header/Header.js
@@ -1,9 +1,76 @@
+import {
+  AppBar,
+  IconButton,
+  Tab,
+  Toolbar,
+  Typography
+} from "@material-ui/core";
+import { Link, Location } from "@reach/router";
+
+import { AccountCircle } from "@material-ui/icons";
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { useAppState } from "../../hooks";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+    flexDirection: "row"
+  },
+  flex: {
+    display: "flex"
+  },
+  selectedTab: {
+    opacity: 1,
+    borderBottom: "1px solid red"
+  }
+});
+
+function NavLinkTab(props) {
+  const classes = useStyles();
+  return (
+    <Location>
+      {({ location }) => {
+        return (
+          <Tab
+            classes={{ selected: classes.selectedTab }}
+            selected={
+              props.to !== "/"
+                ? location.pathname.includes(props.to)
+                : location.pathname === props.to
+            }
+            component={Link}
+            {...props}
+          />
+        );
+      }}
+    </Location>
+  );
+}
 
 function Header() {
   const { account } = useAppState();
-  return <div>Header, {JSON.stringify(account)}</div>;
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static" className={classes.root}>
+      <Toolbar>
+        <Typography variant="h6">Tengai</Typography>
+      </Toolbar>
+
+      <div className={`${classes.flex} ${classes.root}`}>
+        <NavLinkTab label="Dashboard" to="/" />
+        <NavLinkTab label="Assignments" to="/assignments" />
+        <NavLinkTab label="Candidates" to="/candidates" />
+      </div>
+
+      {account && (
+        <IconButton aria-label="account of current user">
+          <AccountCircle />
+        </IconButton>
+      )}
+    </AppBar>
+  );
 }
 
 export default Header;
