b9d905cc05d51bedf7f73352631b343a1a8010c2
Andrew Szucs <andrew0szucs@gmail.com>
2019-11-23 22:32:12 +0100

    Add CSSBaseLine to Layout

--- src/App/pages/Layout/Layout.js
+++ src/App/pages/Layout/Layout.js
@@ -1,9 +1,11 @@
+import { CssBaseline } from "@material-ui/core";
 import Header from "../../components/Header";
 import React from "react";
 
 function Layout(props) {
   return (
     <>
+      <CssBaseline />
       <Header />
       {props.children}
     </>
