f502fd9aa9f655608b82a1230055fd1938722841
Andrew Szucs <andrew0szucs@gmail.com>
2019-11-23 22:31:17 +0100

    Add Links to all the assignment details in the AssignmentListsPage

--- src/App/pages/AssignmentsListPage/AssignmentsListPage.js
+++ src/App/pages/AssignmentsListPage/AssignmentsListPage.js
@@ -1,7 +1,18 @@
+import { Link } from "@reach/router";
 import React from "react";
+import { useAppState } from "../../hooks";
 
 function AssignmentsListPage(props) {
-  return <div>AssignmentsListPage</div>;
+  const { assignments } = useAppState();
+  return (
+    <ul>
+      {assignments.map(({ name, id }) => (
+        <li key={id}>
+          <Link to={`/assignments/${id}`}>{name}</Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default AssignmentsListPage;
