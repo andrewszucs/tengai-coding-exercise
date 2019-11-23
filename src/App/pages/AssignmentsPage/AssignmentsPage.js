eab9f14e475890a5725eff3db6fc8049267adf85
Andrew Szucs <andrew0szucs@gmail.com>
2019-11-23 22:32:49 +0100

    Implement AssignmentsPage

--- src/App/pages/AssignmentsPage/AssignmentsPage.js
+++ src/App/pages/AssignmentsPage/AssignmentsPage.js
@@ -1,10 +1,23 @@
+import AssignmentsSider from "../../components/AssignmentsSider";
 import React from "react";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex"
+  },
+  stretch: {
+    flex: 1
+  }
+});
 
 function AssignmentsPage(props) {
+  const classes = useStyles();
+
   return (
-    <div>
-      AssignmentsPage
-      <div>{props.children}</div>
+    <div className={classes.root}>
+      <AssignmentsSider />
+      <div className={classes.stretch}>{props.children}</div>
     </div>
   );
 }
