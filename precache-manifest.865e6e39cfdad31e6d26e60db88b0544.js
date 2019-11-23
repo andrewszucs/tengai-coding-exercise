a08a91a46131b7d2bfb803ab5412119287794c17
Andrew Szucs <andrew0szucs@gmail.com>
2019-11-23 22:39:41 +0100

    Updates

---
+++ precache-manifest.865e6e39cfdad31e6d26e60db88b0544.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23dd2f16c57c6692ef68c65ed2c0cb6d",
+    "url": "/tengai-coding-exercise/index.html"
+  },
+  {
+    "revision": "4eb33b61e6a0d5543a02",
+    "url": "/tengai-coding-exercise/static/js/2.dd6d8248.chunk.js"
+  },
+  {
+    "revision": "55613280e944e339c473",
+    "url": "/tengai-coding-exercise/static/js/main.3b387739.chunk.js"
+  },
+  {
+    "revision": "7a78e186f5394e1024d8",
+    "url": "/tengai-coding-exercise/static/js/runtime-main.415f2a04.js"
+  }
+]);
